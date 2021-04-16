b489536141de4fd35b2dcc8d6760cdd4fca6884c
zcar35 <47577705+zcar35@users.noreply.github.com>
2021-04-15 22:58:47 -0400

    Add files via upload

--- task1_4_8 (1).c
+++ task1_4_8 (1).c
@@ -23,80 +23,136 @@ void *text(void *arg);
 int counter = 0; 
 
 int code[] = { 0,1,2,3,4,5,6 };
+int sem_wait(sem_t *sem);
+int sem_post(sem_t *sem);
+int pthread_mutex_lock(pthread_mutex_t *mutex);
+int pthread_mutex_unlock(pthread_mutex_t *mutex);
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+sem_t sema[7];
 
-int main()
-{
-  int i;
-  pthread_t tid[7];
-  int sem_wait(sem_t *sem);
-
+int main() {
+    int i, j;
+    pthread_t tid[7];
+    pthread_mutex_t mutex;
+    int ret;
+    int counter;
 
     for (i = 0; i < 7; i++)
-    pthread_create(&tid[i], NULL, text, (void*)&code[i]);
+    {
+    pthread_create(&tid[i], NULL, text, (void *) &code[i]);
+}
+
+    for (j=0;j<7;j++)
+    {
+        pthread_join(tid[j],NULL);
+    }
+
 
   return 0;
 }
 
 void *text(void *arg)
 {
+    int ret;
+    pthread_mutex_lock(&mutex);
+   //pthread_mutex_lock();
 	int n = *(int*)arg;
-	if(n==0)
+
+	if(n!=counter)
     {
-	    sem_wait(0);
+	    if(n==1)
+        {
+            sem_wait(&sema[1]);
+
+        }
+        if (n==2)
+        {
+            sem_wait(&sema[2]);
+
+        }
+        if(n==3)
+        {
+            sem_wait(&sema[3]);
+
+
+        }
+        if(n==4)
+        {
+            sem_wait(&sema[4]);
+
+        }
+        if(n==5)
+        {
+            sem_wait(&sema[5]);
+        }
+        if(n==6)
+        {
+            sem_wait(&sema[6]);
+        }
+        if(n==7)
+        {
+            sem_wait(&sema[7]);
+
+        }
     }
-	sem_wait(&mutex);
-    while (count != n)
+    if (n==counter)
     {
-        sem_wait()
+        counter++;
     }
-		// wait sema0 
-	//wait sema 
-	
-	//while counter != n
-	    // wait cond var 
-  
 
   switch (n)
   {
     case 0:
       printf("A semaphore S is an integer-valued variable which can take only non-negative\n");
       printf("values. Exactly two operations are defined on a semaphore:\n\n");
-      //post sema1
-      //counter++ 
+      sem_post(&sema[1]);
+      //counter++;
       break;
 
     case 1:
       printf("Signal(S): If there are processes that have been suspended on this semaphore,\n");
       printf(" wake one of them, else S := S+1.\n\n");
-      //post sema2
+      sem_post(&sema[2]);
+      //counter++;
       break;
 
     case 2:
       printf("Wait(S): If S>0 then S:=S-1, else suspend the execution of this process.\n");
       printf(" The process is said to be suspended on the semaphore S.\n\n");
+      sem_post(&sema[3]);
+      //counter++;
       break;
 
     case 3:
       printf("The semaphore has the following properties:\n\n");
+      sem_post(&sema[4]);
+      //counter++;
       break;
 
     case 4:
       printf("1. Signal(S) and Wait(S) are atomic instructions. In particular, no\n");
       printf(" instructions can be interleaved between the test that S>0 and the\n");
       printf(" decrement of S or the suspension of the calling process.\n\n");
+      sem_post(&sema[5]);
+      //counter++;
       break;
 
     case 5:
       printf("2. A semaphore must be given an non-negative initial value.\n\n");
+      sem_post(&sema[6]);
+      //counter++;
       break;
 
     case 6:
       printf("3. The Signal(S) operation must waken one of the suspended processes. The\n");
       printf(" definition does not specify which process will be awakened.\n\n");
+      sem_post(&sema[7]);
+      //counter++;
       break;
   }
-   //Post sema
-   // broadcast cond var
-  pthread_exit(0);
+    pthread_mutex_unlock(&mutex);
+
+	//sem_post(sem_t *);
+	pthread_exit(0);
 }
 
--- task2_4_8.c
+++ task2_4_8.c
@@ -6,89 +6,266 @@ typedef struct {
 	int id_num; //id
 	char direct; // direction they are coming from 
 	char right; // which is to the right
-} bat;
+	int bats;
+}
+bat;
+ //struct bat getInfo()
+//{
+   // struct bats b;
+   // printf("Enter number of bats");
+   // scanf("%d");
+   // return b;
+//}
+//here I was trying to create a struct for the bats input.. just couldn't figure it out :(
 
 //One mutex to get into ALL functions 
 pthread_mutex_t mutex;
 
 //Some counters: 
 int total = 0; //total at intersections
-int northBats = 0; 
+int northBats = 0;
+int southBats = 0;
+int eastBats = 0;
+int westBats = 0;
 //...
+int pthread_mutex_lock(pthread_mutex_t *mutex);
+int pthread_mutex_unlock(pthread_mutex_t *mutex);
+void *goBatGo(void *arg);
+void *doTheRest(void *arg);
+void *arrive(void *arg);
+void *cross(void *arg);
+void *leave(void *arg);
+void *check(void *arg);
+pthread_cond_t eastQueue;
+pthread_cond_t westQueue;
+pthread_cond_t northQueue;
+pthread_cond_t southQueue;
+pthread_cond_t northFirst;
+pthread_cond_t southFirst;
+pthread_cond_t eastFirst;
+pthread_cond_t westFirst;
+
 
 int main(int argc, char **argv){
 	//Let's safely assume that we will at most have 
 	//100 BATs
-	char directions[100]; 
-	strcpy(directions, argv[1]);
+	char directions[100];
+	char  bats[100];
+	pthread_mutex_t tid[100];
+	//int a = getInfo(); here was supposed to go the struct
+    pthread_mutex_t mutex;
+
+    strcpy(directions, argv[1]);
 	printf("Argc: %i and directions %s\n", argc, directions);
 	//1) loop through input (strlen(directions)) and 
 	//   create array of bat structs
-	
+	for(int i=0;i<strlen(directions);i++)
+    {
+	   // struct bats[i];
+	   //basically here I was trying to come up with a way to populate the bats..
+
+    }
+
+    for (int i = 0; i <100; ++i)
+    {
+        pthread_create(&tid[i], NULL, goBatGo, (void *) &bats[i]);
+
+    }
+
+
+
 	
 	//2) loop through bats now and dispatch all of
 	//   the threads 
 	// maybe send to a function that takes in a 
 	// bat struct... 
 	
-	return 0; 
+	return 0;
 }
+
 
 void *goBatGo(void *arg){
 	bat *b = (bat *) arg; 
-	//check(b);
-	//doTheRest(b);
+	check(b);
+	doTheRest(b);
 }
 
-void doTheRest(void *arg){
-	//arrive(b)
-	//cross(b)
-	//leave(b)
+void doTheRest(void *arg){//not really sure why I get an error here....
+	arrive(b);
+	cross(b);
+	leave(b);
 }
 
 void *arrive(void *arg)
 {
 	bat *b = (bat *) arg; 
-	//lock
-	//if b->direct == 'n' 
-	//    northBats++
-	//    if northBats > 1
-	//       pthread_cond_wait(&northQueue, &mutex)
+	pthread_mutex_lock(&mutex);
+	if (b->direct == 'n')
+    {
+	    northBats++;
+    }
+	    if (northBats > 1)
+        {
+	        pthread_cond_wait(&northQueue, &mutex);
+
+
+        }
+    if (b->direct == 's')
+    {
+        southBats++;
+    }
+    if (southBats > 1)
+    {
+        pthread_cond_wait(&southQueue, &mutex);
+
+
+    }
+    if (b->direct == 'w')
+    {
+        westBats++;
+    }
+    if (westBats > 1)
+    {
+        pthread_cond_wait(&westQueue, &mutex);
+
+
+    }
+    if (b->direct == 'e')
+    {
+        eastBats++;
+    }
+    if (eastBats > 1)
+    {
+        pthread_cond_wait(&eastQueue, &mutex);
+
+
+    }
 	//...
-	//total++
-	//unlock
+	total++;
+	pthread_mutex_unlock(&mutex);
 }
 
 void *cross(void *arg)
 {
-	bat *b = (bat *) arg; 
-	//lock
-	//if b->right == 'n' && northBats > 0 
-	      //pthread_cond_wait(&northFirst, &mutex); 
+	bat *b = (bat *) arg;
+    pthread_mutex_lock(&mutex);
+    if (b->right == 'n' && northBats > 0)
+    {
+        pthread_cond_wait(&northFirst, &mutex);
+
+
+    }
+    if (b->direct == 'n')
+    {
+        northBats--;
+        pthread_cond_signal(&northQueue);
+
+    }
+
+    if (b->right == 'e' && eastBats > 0)
+    {
+        pthread_cond_wait(&eastFirst, &mutex);
+
+
+    }
+    //...
+    if (b->direct == 'w')
+    {
+        eastBats--;
+        pthread_cond_signal(&westQueue);
+
+    }
+    if (b->right == 'w' && westBats > 0)
+    {
+        pthread_cond_wait(&westFirst, &mutex);
+
+
+    }
+    if (b->right == 's' && southBats > 0)
+    {
+        pthread_cond_wait(&southFirst, &mutex);
+
+
+    }
+    if (b->direct == 's')
+    {
+        northBats--;
+        pthread_cond_signal(&southQueue);
+
+    }
+    //...
+
 	//...
-	//if b->direction == 'n'
-	//   northBats--
-	//   pthread_cond_signal(&northQueue);
-	//...
-	//total-- 
-	//unlock
+	total-- ;
+    pthread_mutex_unlock(&mutex);
+
 }
 
 void *leave(void *arg)
 {
 	bat *b = (bat *) arg;
-	//lock 
-	//if b->direct == 'n'
-	//    pthread_cond_signal(&northFirst)
+    pthread_mutex_lock(&mutex);
+
+    if (b->direct == 'n')
+    {
+        pthread_cond_signal(&northFirst);
+
+
+    }
+    if (b->direct == 's')
+    {
+        pthread_cond_signal(&southFirst);
+
+
+    }
+    if (b->direct == 'e')
+    {
+        pthread_cond_signal(&eastFirst);
+
+
+    }
+    if (b->direct == 'w')
+    {
+        pthread_cond_signal(&westFirst);
+
+
+    }
 	//...
-	
-	//unlock
+    pthread_mutex_unlock(&mutex);
+
+
+
 }
 void *check(void *arg)
 {
 	bat *b = (bat *) arg;
-	//if total == 4
-	//   if b->direct == 'n' 
-	//       pthread_cond_signal(&northFirst)
+	if (total == 4)
+    {
+        if (b->direct == 'n')
+        {
+            pthread_cond_signal(&northFirst);
+
+        }
+        if (b->direct == 's')
+        {
+            pthread_cond_signal(&southFirst);
+
+        }
+        if (b->direct == 'e')
+        {
+            pthread_cond_signal(&eastFirst);
+
+        }
+        if (b->direct == 'n')
+        {
+            pthread_cond_signal(&eastFirst);
+
+        }
+        //return b;
+
+
+
+    }
+
 }
 
